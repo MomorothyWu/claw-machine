b01b9f2048bb2b51b4e8d0ce833f7de17144d048
MomorothyWu <gifted2815@gmail.com>
2025-05-29 14:11:00 +0800

    0529_1

---
+++ src/app/fb0529/page.js
@@ -0,0 +1,24 @@
+"use client"
+import { initializeApp } from "firebase/app";
+
+export default function FB0529() {
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDv7q1pGeQP2Tupw_6NQxxiGfnU5LJAzjQ",
+    authDomain: "nccu-204012.firebaseapp.com",
+    projectId: "nccu-204012",
+    storageBucket: "nccu-204012.firebasestorage.app",
+    messagingSenderId: "371171216729",
+    appId: "1:371171216729:web:384fdc3b07872a25d99048"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  return (
+    <>
+      fb0529
+    </>
+  );
+}
